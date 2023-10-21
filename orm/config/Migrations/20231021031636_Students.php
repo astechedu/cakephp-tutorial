bfbc1bf258cd0611f3ac766f70306508fa527b55
ajay <ajay@ubuntu22.04.myguest.virtualbox.org>
2023-10-21 10:21:01 +0530

    CakePHP5 Database ORM Ex.

---
+++ orm/config/Migrations/20231021031636_Students.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class Students extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change(): void
+    {
+        $table = $this->table('students');
+        $table->addColumn('name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => true,
+        ]);
+        $table->addColumn('description', 'text', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => true,
+        ]);
+        $table->create();         
+    }
+}
